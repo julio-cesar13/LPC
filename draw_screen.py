19bfdb536dd07a31f9e679c419de19f72e70fd7f
julio-cesar13 <120072953+julio-cesar13@users.noreply.github.com>
2023-01-23 11:29:06 -0400

    Add files via upload

---
+++ draw_screen.py
@@ -0,0 +1,33 @@
+from blocks import blocks
+from config import *
+
+
+def draw_object(player):
+    # draw L left
+    list_reacts = []
+    for cor in blocks(num_arena):
+        aux = pygame.draw.rect(screen, "#c29f2e", (cor[1] * 25, cor[0] * 25 + 50, 25, 25))
+        aux = aux.colliderect(player)
+        list_reacts.append(aux)
+    # draw screen
+    screen_1 = pygame.draw.rect(screen, "#c29f2e", (0, 50, 1000, 25))
+    screen_1 = screen_1.colliderect(player)
+    list_reacts.append(screen_1)
+
+    screen_2 = pygame.draw.rect(screen, "#c29f2e", (0, 725, 1000, 25))
+    screen_2 = screen_2.colliderect(player)
+    list_reacts.append(screen_2)
+
+    screen_3 = pygame.draw.rect(screen, "#c29f2e", (975, 75, 25, 650))
+    screen_3 = screen_3.colliderect(player)
+    list_reacts.append(screen_3)
+
+    screen_4 = pygame.draw.rect(screen, "#c29f2e", (0, 75, 25, 650))
+    screen_4 = screen_4.colliderect(player)
+    list_reacts.append(screen_4)
+
+    # collision detection
+    if True in list_reacts:
+        return False
+    else:
+        return True
---
+++ move.py
@@ -0,0 +1,45 @@
+import pygame
+import math
+
+
+def load_images(sprite, ang, w, h, xp, yp, image):
+    sprite.image = pygame.image.load(image)
+    sprite.image = pygame.transform.scale(sprite.image, (w, h))
+    sprite.image = pygame.transform.rotate(sprite.image, ang)
+    sprite.rect = sprite.image.get_rect(center=(xp + 25, yp + 25))
+
+
+def move_left(keys, permission_1, xp1, yp1, ang_left):
+    # Move p1
+    if keys[pygame.K_w] and permission_1:
+        xp1 += math.cos(math.radians(ang_left))
+        yp1 -= math.sin(math.radians(ang_left))
+        return xp1, yp1, ang_left
+
+    elif keys[pygame.K_a]:
+        ang_left += 1
+        return xp1, yp1, ang_left
+
+    elif keys[pygame.K_d]:
+        ang_left += -1
+        return xp1, yp1, ang_left
+    else:
+        return False
+
+
+def move_right(keys, permission_2, xp2, yp2, ang_right):
+    # Move p2
+    if keys[pygame.K_UP] and permission_2:
+        xp2 += math.cos(math.radians(ang_right + 180))
+        yp2 -= math.sin(math.radians(ang_right + 180))
+        return xp2, yp2, ang_right
+
+    elif keys[pygame.K_LEFT]:
+        ang_right += 1
+        return xp2, yp2, ang_right
+
+    elif keys[pygame.K_RIGHT]:
+        ang_right += -1
+        return xp2, yp2, ang_right
+    else:
+        return False
